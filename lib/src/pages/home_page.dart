89b87dd94ee4cefc707e2de52f0d4e903b147c85
marcoestrada-17 <javier.itza.est@qroo.nuevaescuela.mx>
2022-05-22 18:33:31 -0500

    Uso de franData base en visual

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
 
-//cambiar id de la aplicacion y ajustes
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
